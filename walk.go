5553399bc8fa547550a1797e3a1a9ecdb553c54a
Wojciech Adam Koszek <wojciech@koszek.com>
2016-11-24 12:32:27 -0800

    bring a more useful walking program

--- walk.go
+++ walk.go
@@ -2,21 +2,39 @@ package main
 
 import (
     "fmt"
+    "time"
     "os"
+    "log"
     "path/filepath"
 )
 
 func main() {
-    searchDir := "/Users/wk/r/me/"
 
-    fileList := []string{}
-    err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-        fileList = append(fileList, path)
-        return nil
-    })
 
-    for _, file := range fileList {
-        fmt.Println(file)
-    }
-    fmt.Println(err)
+	if (len(os.Args) != 2) {
+		log.Fatal("bleh")
+	}
+	searchDir := os.Args[1]
+
+	timeBefore := time.Now()
+	fileList := []string{}
+		err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return nil
+	})
+	timeAfter := time.Now()
+
+	fmt.Println("============= Time difference ============")
+	diff := timeAfter.Sub(timeBefore)
+	fmt.Println(diff)
+
+	fmt.Println("=========== Will wait here ==============")
+	time.Sleep(3000*time.Second)
+
+	if (false) {
+		for _, file := range fileList {
+			fmt.Println(file)
+		}
+		fmt.Println(err)
+	}
 }
