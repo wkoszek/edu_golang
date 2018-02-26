d3b0af5641beaad3d3961edb47fd0aaad2fdb339
Wojciech Adam Koszek <wojciech@koszek.com>
2018-02-25 23:51:50 -0800

    Changes

--- readyaml.go
+++ readyaml.go
@@ -18,7 +18,6 @@ type Post struct {
 	Layout	   string	`yaml:"layout"`
 	Tags	 []string	`yaml:"tags"`
 	Ads	 []string	`yaml:"ads"`
-
 	Body	 []string
 }
 
@@ -29,22 +28,29 @@ const (
 
 var (
 	err		  error
-	posts_all	[Max_posts_number]Post
+	posts_all	[]Post
 )
 
 func main() {
 	var files []string
+	var p Post
 
 	files, err = filepath.Glob(Posts_path)
 	failonerr(err)
 
-	var p Post
-	for pi, filename := range(files) {
+	for _, filename := range(files) {
 		p = processfile(filename)
-		posts_all[pi] = p
+		posts_all = append(posts_all, p)
 	}
 
+	renderAll(posts_all)
 	os.Exit(0)
+}
+
+func failonerr(err error) {
+	if (err != nil) {
+		log.Fatal(err)
+	}
 }
 
 func processfile(filename string) Post {
@@ -72,16 +78,17 @@ readyaml(filename string, body []byte) Post {
 	err := yaml.Unmarshal(body, &p)
 	failonerr(err)
 
-	if (false) {
+	if (true) {
 		fmt.Println(filename)
 		fmt.Printf("--- t:\n%+v\n\n", p)
 	}
 	return p
 }
 
-func failonerr(err error) {
-	if (err != nil) {
-		log.Fatal(err)
-	}
+func renderAll(allPosts []Post) bool {
+//	tmpl, err := template.New("main").Parse("{{.Count}} items are made of {{.Material}}")
+//	if err != nil { panic(err) }
+//	err = tmpl.Execute(os.Stdout, sweaters)
+//	if err != nil { panic(err) }
+	return true
 }
-
